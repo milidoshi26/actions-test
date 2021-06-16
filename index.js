f8ab7482841af93cb0d240234506477ebb729d2a
Mili Doshi <67476043+milidoshi26@users.noreply.github.com>
2021-06-16 19:41:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const github = require('@actions/github');
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}! "Mongo Connection string mongodb://mongodb-example:erSNrY3Ucc3Q1v3JzDbfvRDiwj7n082WikBhX6C0VZa8lddDrqQN2yYiHpNoXhFLcsrOtmsK5bcVPriIWIt8KQ==@example-example.documents.azure.com:10255/store?ssl=true&sslverifycertificate=false&replicaSet=globaldb&connectTimeoutMS=300000&socketTimeoutMS=300000&retryWrites=true"
-`);
+  console.log(`Hello ${nameToGreet}! xoxa\-g76xi\a\fo3s1s0f4rnlha1wagmgskqns6zw15dr69yi2ld\as2qemu21kxmby2xv1q9k9kbtllfslcep`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
