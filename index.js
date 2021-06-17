790c0b83a5add7973021e85741178f0267d74563
Mili Doshi <67476043+milidoshi26@users.noreply.github.com>
2021-06-17 11:10:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const github = require('@actions/github');
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}! \-----BEGIN PRIVATE KEY\-----\1y\6k3oh7ol0cugt6mvhbe77tm+5eg08a33gsjd5iken`);
+  console.log(`Hello ${nameToGreet}! \-----BEGIN PRIVATE KEY\-----nzb8mzov5oyt+syfloqbibsn0rm791f8f3m/1mafvv296`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
