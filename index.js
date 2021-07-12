b9a03e9ef6615552148e4506a0491b8d4f434979
Mili Doshi <67476043+milidoshi26@users.noreply.github.com>
2021-07-12 14:11:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const path = require('path');
 
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}! Slack token: xoxp\-sut9wktw962fpm25l-p1q\plocdvar8tjfkmj\y0b0miu3e8142mhz2oi23dfu1oylf5alzj8q64xpzjk0rr0tw1g5a7`);
+  console.log(__dirname);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
